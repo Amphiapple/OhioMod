4d2b6591c997f55caf924f6466d546d86ebf455b
Amphiapple <chiuwalter2003@gmail.com>
2024-08-10 17:47:30 -0700

    change version

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace OhioMod;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "44.1";
-    public const string Version = "44.1.0";
+    public const string Version = "44.1.2";
     public const string Name = "OhioMod";
 
     public const string Description = "BTD6 but in Ohio";
