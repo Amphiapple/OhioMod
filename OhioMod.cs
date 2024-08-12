698fecd52ccce0ad7ababcdc326c053955116d17
Amphiapple <chiuwalter2003@gmail.com>
2024-08-11 21:41:45 -0700

    add tests for most basic stat changes

--- OhioMod.cs
+++ OhioMod.cs
@@ -30,6 +30,11 @@ using Il2CppAssets.Scripts.Models.Towers.Projectiles.Behaviors;
 using UnityEngine.UIElements;
 using Il2CppAssets.Scripts.Unity.Towers;
 using Il2CppAssets.Scripts.Unity.Towers.Upgrades;
+using System.Text.RegularExpressions;
+using Il2CppAssets.Scripts.Models.Towers.Projectiles;
+using System.Linq;
+using Il2CppAssets.Scripts.Models.Towers.Weapons.Behaviors;
+using Il2CppAssets.Scripts.Models.Towers.Behaviors.Abilities.Behaviors;
 
 
 [assembly: MelonInfo(typeof(OhioMod.OhioMod), OhioMod.ModHelperData.Name, OhioMod.ModHelperData.Version, OhioMod.ModHelperData.RepoOwner)]
@@ -52,31 +57,88 @@ public class OhioMod : BloonsTD6Mod
         [HarmonyPostfix]
         public static void Postfix()
         {
+
             var models = Game.instance.model;
 
             foreach (TowerModel tower in Game.instance.model.towers)
             {
+                //Duration and Cooldown tester
+                if (Regex.IsMatch(tower.name, "TackShooter-.5."))
+                {
+                    foreach (var a in tower.GetAbilities())
+                    {
+                        a.cooldown *= 2;
+                        foreach (var b in a.behaviors)
+                        {
+                            try
+                            {
+                                b.Cast<ActivateAttackModel>().lifespan *= 4;
+                                
+                            }
+                            catch
+                            {
+
+                            }
+                            
+                        }
+                    }
+                }
+
+                //Damage tester
+                if (Regex.IsMatch(tower.name, "DartMonkey-..[3-5]"))
+                {
+                    foreach (var w in tower.GetWeapons())
+                    {
+                        w.projectile.GetDamageModel().damage *= 10;
+                    }
+                }
+                if (Regex.IsMatch(tower.name, "DartMonkey-..[4-5]"))
+                {
+                    foreach (var w in tower.GetWeapons())
+                    {
+                        foreach (var b in w.behaviors)
+                        {
+                            try
+                            {
+                                b.Cast<CritMultiplierModel>().damage *= 10;
+                            }
+                            catch
+                            {
+
+                            }
+                        }
+                        
+                    }
+                }
+
+                //Pierce tester
+                if (Regex.IsMatch(tower.name, "DartMonkey-[1-5].."))
+                {
+                    foreach (var w in tower.GetWeapons())
+                    {
+                        w.projectile.pierce += 100;
+                    }
+                }
+
+                //Rate tester
+                if (Regex.IsMatch(tower.name, "DartMonkey-.[1-5]."))
+                {
+                    foreach (var w in tower.GetWeapons())
+                    {
+                        w.rate *= 0.1f;
+                    }
+                }
 
                 //Range tester
-                if (tower.baseId == "IceMonkey" && tower.HasUpgrade(1, 3))
+                if (Regex.IsMatch(tower.name, "DartMonkey-..[1-5]"))
                 {
-                    tower.range *= (float)1.5;
+                    tower.range *= 1.5f;
 
                     foreach (var bev in tower.behaviors)
                     {
                         try
                         {
-                            bev.Cast<FreezeNearbyWaterModel>().radius *= (float)1.5;
-
-                        }
-                        catch
-                        {
-
-                        }
-                        try
-                        {
-                            bev.Cast<AttackModel>().range *= (float)1.5;
-
+                            bev.Cast<AttackModel>().range *= 1.5f;
                         }
                         catch
                         {
@@ -85,72 +147,12 @@ public class OhioMod : BloonsTD6Mod
 
                     }
                 }
+                
             }
 
-            //Cost Changes
-            models.GetTowerFromId("Mermonkey").cost = 400;
-            models.GetTowerFromId("CaptainChurchill").cost = 1000;
-            models.GetTowerFromId("Corvus").cost = 2000;
-            models.GetUpgrade("Super Monkey Fan Club").cost = 3000;
+            //Cost tester
+            models.GetTowerFromId("DartMonkey").cost = 100;
             models.GetUpgrade("Plasma Monkey Fan Club").cost = 35000;
-            models.GetUpgrade("Crossbow Master").cost = 20000;
-            models.GetUpgrade("Glaive Lord").cost = 32000;
-            models.GetUpgrade("MOAB Eliminator").cost = 25000;
-            models.GetUpgrade("Snowstorm").cost = 3000;
-            models.GetUpgrade("Absolute Zero").cost = 16000;
-            models.GetUpgrade("Cryo Cannon").cost = 1750;
-            models.GetUpgrade("Icicles").cost = 2000;
-            models.GetUpgrade("The Bloon Solver").cost = 25000;
-            models.GetUpgrade("Glue Storm").cost = 15000;
-            models.GetUpgrade("MOAB Glue").cost = 4000;
-            models.GetUpgrade("Elite Sniper").cost = 12000;
-            models.GetUpgrade("Elite Defender").cost = 12000;
-            models.GetUpgrade("Energizer").cost = 28000;
-            models.GetUpgrade("Buccaneer-Carrier Flagship").cost = 26000;
-            models.GetUpgrade("Ground Zero").cost = 14000;
-            models.GetUpgrade("Tsar Bomba").cost = 32000;
-            models.GetUpgrade("Neva-Miss Targeting").cost = 2000;
-            models.GetUpgrade("Spectre").cost = 24000;
-            models.GetUpgrade("Flying Fortress").cost = 75000;
-            models.GetUpgrade("Apache Dartship").cost = 13000;
-            models.GetUpgrade("Apache Prime").cost = 50000;
-            models.GetUpgrade("Downdraft").cost = 2500;
-            models.GetUpgrade("Support Chinook").cost = 5000;
-            models.GetUpgrade("Special Poperations").cost = 25000;
-            models.GetUpgrade("MOAB Shove").cost = 3500;
-            models.GetUpgrade("Shattering Shells").cost = 7000;
-            models.GetUpgrade("Laser Cannon").cost = 3000;
-            models.GetUpgrade("Plasma Accelerator").cost = 9000;
-            models.GetUpgrade("Hydra Rocket Pods").cost = 4000;
-            models.GetUpgrade("Rocket Storm").cost = 4000;
-            models.GetUpgrade("Buckshot").cost = 0;
-            models.GetUpgrade("Bloon Exclusion Zone").cost = 60000;
-            models.GetUpgrade("Summon Phoenix").cost = 4500;
-            models.GetUpgrade("Laser Blasts").cost = 1000;
-            models.GetUpgrade("Sun Avatar").cost = 17500;
-            models.GetUpgrade("Knockback").cost = 4000;
-            models.GetUpgrade("Bloon Sabotage").cost = 6000;
-            models.GetUpgrade("Transforming Tonic").cost = 3500;
-            models.GetUpgrade("Total Transformation").cost = 35000;
-            models.GetUpgrade("Bloon Master Alchemist").cost = 20000;
-            models.GetUpgrade("Abyss Dweller").cost = 1000;
-            models.GetUpgrade("Abyssal Warrior").cost = 3000;
-            models.GetUpgrade("Lord of the Abyss").cost = 22000;
-            models.GetUpgrade("Riptide Champion").cost = 555;
-            models.GetUpgrade("Arctic Knight").cost = 5555;
-            models.GetUpgrade("Popseidon").cost = 55555;
-            models.GetUpgrade("Alluring Melody").cost = 1400;
-            models.GetUpgrade("Symphonic Resonance").cost = 6000;
-            models.GetUpgrade("Long Life Spikes").cost = 800;
-            models.GetUpgrade("Deadly Spikes").cost = 2500;
-            models.GetUpgrade("Perma-Spike").cost = 32000;
-            models.GetUpgrade("Monkey Intelligence Bureau").cost = 4000;
-            models.GetUpgrade("Homeland Defense").cost = 30000;
-            models.GetUpgrade("Sentry Paragon").cost = 28000;
-            models.GetUpgrade("Ultraboost").cost = 37000;
-            models.GetUpgrade("Megalodon").cost = 30000;
-            models.GetUpgrade("Giant Condor").cost = 12500;
-            models.GetUpgrade("Pouakai").cost = 12500;
             
         }
     }
